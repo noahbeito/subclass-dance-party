a7ead0448304f4c8cd47e61c461f9da04cfe1c32
Noah Beito <noah.beito@gmail.com>
2023-04-04 18:26:38 -0700

    no updates to blinkyDancer.js

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,8 +1,10 @@
 var MakeBlinkyDancer = function(top, left, timeBetweenSteps) {
+
   MakeDancer.call(this, top, left, timeBetweenSteps);
 
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
+
 };
 
 MakeBlinkyDancer.prototype = Object.create(MakeDancer.prototype);
